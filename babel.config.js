c018014bdc67ad6c0765a5a88db8d97df6d59562
unique.mo <myx3901051@qq.com>
2019-06-12 11:53:06 +0800

    fix: babel config for build es6 class

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,8 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: {
-          node: 'current'
+          node: 'current',
+          browsers: ['last 2 versions']
         }
       }
     ],
--- es/index.js
+++ es/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
+function _classCallCheck(instance, Constructor) {
+  if (!(instance instanceof Constructor)) {
+    throw new TypeError("Cannot call a class as a function");
+  }
+}
+
+function _defineProperties(target, props) {
+  for (var i = 0; i < props.length; i++) {
+    var descriptor = props[i];
+    descriptor.enumerable = descriptor.enumerable || false;
+    descriptor.configurable = true;
+    if ("value" in descriptor) descriptor.writable = true;
+    Object.defineProperty(target, descriptor.key, descriptor);
+  }
+}
+
+function _createClass(Constructor, protoProps, staticProps) {
+  if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+  if (staticProps) _defineProperties(Constructor, staticProps);
+  return Constructor;
+}
+
 function _defineProperty(obj, key, value) {
   if (key in obj) {
     Object.defineProperty(obj, key, {
@@ -35,45 +57,107 @@ function _extends() {
   return _extends.apply(this, arguments);
 }
 
+function _inherits(subClass, superClass) {
+  if (typeof superClass !== "function" && superClass !== null) {
+    throw new TypeError("Super expression must either be null or a function");
+  }
+
+  subClass.prototype = Object.create(superClass && superClass.prototype, {
+    constructor: {
+      value: subClass,
+      writable: true,
+      configurable: true
+    }
+  });
+  if (superClass) _setPrototypeOf(subClass, superClass);
+}
+
+function _getPrototypeOf(o) {
+  _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) {
+    return o.__proto__ || Object.getPrototypeOf(o);
+  };
+  return _getPrototypeOf(o);
+}
+
+function _setPrototypeOf(o, p) {
+  _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+    o.__proto__ = p;
+    return o;
+  };
+
+  return _setPrototypeOf(o, p);
+}
+
+function _assertThisInitialized(self) {
+  if (self === void 0) {
+    throw new ReferenceError("this hasn't been initialised - super() hasn't been called");
+  }
+
+  return self;
+}
+
+function _possibleConstructorReturn(self, call) {
+  if (call && (typeof call === "object" || typeof call === "function")) {
+    return call;
+  }
+
+  return _assertThisInitialized(self);
+}
+
 /**
  * This function generates the HOC function that will call
  * the handleRoutedChanged function of the DecoratedComponent instance
  * when the route of the browser has been changed.
  */
 
-const onRouteChangedHOC = (DecoratedComponent, config = {
-  mounted: false,
-  onlyPathname: true
-}) => {
+var onRouteChangedHOC = function onRouteChangedHOC(DecoratedComponent) {
+  var config = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {
+    mounted: false,
+    onlyPathname: true
+  };
   config.mounted = config.mounted === undefined ? false : config.mounted;
   config.onlyPathname = config.onlyPathname === undefined ? true : config.onlyPathname;
-  const componentName = DecoratedComponent.displayName || DecoratedComponent.name || 'Component';
-  const isReactComponent = DecoratedComponent.prototype.isReactComponent;
+  var componentName = DecoratedComponent.displayName || DecoratedComponent.name || 'Component';
+  var isReactComponent = DecoratedComponent.prototype.isReactComponent;
 
-  class RouteChangedComponent extends React.Component {
-    constructor(...args) {
-      super(...args);
+  var RouteChangedComponent =
+  /*#__PURE__*/
+  function (_React$Component) {
+    _inherits(RouteChangedComponent, _React$Component);
 
-      _defineProperty(this, "__getHandleRouteChangedFunc", () => {
-        let handleRouteChanged;
+    function RouteChangedComponent() {
+      var _getPrototypeOf2;
+
+      var _this;
+
+      _classCallCheck(this, RouteChangedComponent);
+
+      for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
+        args[_key] = arguments[_key];
+      }
+
+      _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(RouteChangedComponent)).call.apply(_getPrototypeOf2, [this].concat(args)));
+
+      _defineProperty(_assertThisInitialized(_this), "__getHandleRouteChangedFunc", function () {
+        var handleRouteChanged;
 
         if (!isReactComponent && typeof config.handleRouteChanged === 'function') {
           handleRouteChanged = config.handleRouteChanged;
         }
 
-        if (isReactComponent && typeof this.instanceRef.handleRouteChanged === 'function') {
-          handleRouteChanged = this.instanceRef.handleRouteChanged;
+        if (isReactComponent && typeof _this.instanceRef.handleRouteChanged === 'function') {
+          handleRouteChanged = _this.instanceRef.handleRouteChanged;
         }
 
         return handleRouteChanged;
       });
 
-      _defineProperty(this, "__routeChangedHandler", (prevLocation, nextLocation) => {
-        const isSamePath = prevLocation.pathname === nextLocation.pathname;
-        const isSameSearch = prevLocation.search === nextLocation.search;
-        const isSameHash = prevLocation.hash === nextLocation.hash;
+      _defineProperty(_assertThisInitialized(_this), "__routeChangedHandler", function (prevLocation, nextLocation) {
+        var isSamePath = prevLocation.pathname === nextLocation.pathname;
+        var isSameSearch = prevLocation.search === nextLocation.search;
+        var isSameHash = prevLocation.hash === nextLocation.hash;
 
-        const handleRouteChanged = this.__getHandleRouteChangedFunc();
+        var handleRouteChanged = _this.__getHandleRouteChangedFunc();
 
         if (!isSamePath) {
           return handleRouteChanged(prevLocation, nextLocation);
@@ -83,48 +167,58 @@ const onRouteChangedHOC = (DecoratedComponent, config = {
           return handleRouteChanged(prevLocation, nextLocation);
         }
       });
+
+      return _this;
     }
 
-    componentDidMount() {
-      const handleRouteChanged = this.__getHandleRouteChangedFunc();
+    _createClass(RouteChangedComponent, [{
+      key: "componentDidMount",
+      value: function componentDidMount() {
+        var handleRouteChanged = this.__getHandleRouteChangedFunc();
 
-      if (typeof handleRouteChanged !== 'function') {
-        throw new Error('WrappedComponent lacks a handleRouteChanged(prevLocation, currLocation) for processing route changed event.');
+        if (typeof handleRouteChanged !== 'function') {
+          throw new Error('WrappedComponent lacks a handleRouteChanged(prevLocation, currLocation) for processing route changed event.');
+        }
+
+        if (config.mounted) {
+          // Since we have no idea what the previous route is when the component got mounted,
+          // We will pass an null as the prevLocation param
+          handleRouteChanged(null, this.props.location);
+        }
       }
+    }, {
+      key: "componentWillReceiveProps",
+      value: function componentWillReceiveProps(nextProps) {
+        var prevLocation = this.props.location;
+        var nextLocation = nextProps.location;
 
-      if (config.mounted) {
-        // Since we have no idea what the previous route is when the component got mounted,
-        // We will pass an null as the prevLocation param
-        handleRouteChanged(null, this.props.location);
+        if (prevLocation === nextLocation) {
+          return;
+        }
+
+        this.__routeChangedHandler(prevLocation, nextLocation);
       }
-    }
+    }, {
+      key: "render",
+      value: function render() {
+        var _this2 = this;
 
-    componentWillReceiveProps(nextProps) {
-      const prevLocation = this.props.location;
-      const nextLocation = nextProps.location;
+        var props = _extends({}, this.props);
 
-      if (prevLocation === nextLocation) {
-        return;
+        if (isReactComponent) {
+          props.ref = function (ref) {
+            _this2.instanceRef = ref;
+          };
+        }
+
+        return React.createElement(DecoratedComponent, props);
       }
+    }]);
 
-      this.__routeChangedHandler(prevLocation, nextLocation);
-    }
+    return RouteChangedComponent;
+  }(React.Component);
 
-    render() {
-      let props = _extends({}, this.props);
-
-      if (isReactComponent) {
-        props.ref = ref => {
-          this.instanceRef = ref;
-        };
-      }
-
-      return React.createElement(DecoratedComponent, props);
-    }
-
-  }
-
-  _defineProperty(RouteChangedComponent, "displayName", `OnRouteChanged(${componentName})`);
+  _defineProperty(RouteChangedComponent, "displayName", "OnRouteChanged(".concat(componentName, ")"));
 
   _defineProperty(RouteChangedComponent, "propTypes", {
     location: PropTypes.object.isRequired
